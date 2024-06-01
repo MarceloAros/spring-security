951071faa8f47638b98bff1a5713dbab5a1108c8
Marcelo Aros <marcelo.a.aros@gmail.com>
2024-06-01 18:37:59 -0400

    remove unused imports

--- src/main/java/cl/marceloaros/learn/springsecurity/configuration/SecurityConfig.java
+++ src/main/java/cl/marceloaros/learn/springsecurity/configuration/SecurityConfig.java
@@ -2,7 +2,6 @@ package cl.marceloaros.learn.springsecurity.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -17,7 +16,6 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
