0489e1fd371891f7749866ecb68612ef457520e3
Marcelo Aros <marcelo.a.aros@gmail.com>
2024-06-01 12:54:53 -0400

    add 2 endpoints

--- src/main/java/cl/marceloaros/learn/springsecurity/controllers/InitialController.java
+++ src/main/java/cl/marceloaros/learn/springsecurity/controllers/InitialController.java
@@ -1,4 +1,4 @@
-package controllers;
+package cl.marceloaros.learn.springsecurity.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
